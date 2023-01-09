5b8f87494c546942171a9073ffa46e063a213719
Vijay M <vijaymurugan177@gmail.com>
2023-01-09 18:23:22 +0530

    deploy commit

--- pages/list/[metadata_id].jsx
+++ pages/list/[metadata_id].jsx
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Wallet, Network, Chain } from "mintbase";
-import { MintbaseNFT } from "../../components/MintBaseNFT";
 
 const List = () => {
   const router = useRouter();
