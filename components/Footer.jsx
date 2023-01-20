c4cdca282b63f31f42bd67d617e664bb8694ebdb
Vijay M <vijaymurugan177@gmail.com>
2023-01-20 15:06:29 +0530

    small update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,7 +21,7 @@ export const Footer = () => {
               <div className="col-lg-6">
                 <div className="newsletter-part">
                   <div className="ft-header">
-                    <h4>Get The Latest Rarible Updates</h4>
+                    <h4>Get The Latest Updates</h4>
                   </div>
                   <form action="#">
                     <input type="email" placeholder="Your Mail Address" />
@@ -225,7 +225,6 @@ export const Footer = () => {
           </div>
         </div>
       </div>
-
     </footer>
   );
 };
