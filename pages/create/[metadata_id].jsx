a2825ee5f135a5b6f524f9f3dccd5a7922a08177
Vijay M <vijaymurugan177@gmail.com>
2023-03-23 16:34:24 +0530

    UPDATED :  ENV variables

--- pages/create/[metadata_id].jsx
+++ pages/create/[metadata_id].jsx
@@ -5,11 +5,6 @@ import { useWallet } from "@mintbase-js/react";
 import axios from "axios";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  "https://rqechictkuaydahokxpn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxZWNoaWN0a3VheWRhaG9reHBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg1NDIyNTQsImV4cCI6MTk5NDExODI1NH0._8dFzAKiPBe0sX-bIfgeAU5Zvuk5Q8o8Ju69uH957iQ"
-);
-
 const UploadFiles = () => {
   const [nftData, setNftData] = useState();
   const [collectionImages, setCollectionImages] = useState();
@@ -63,6 +58,11 @@ const UploadFiles = () => {
     }
     fetchCheckNFT();
   }, [activeAccountId]);
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_PROJECT_API_KEY
+  );
 
   const uploadFiles = async (e) => {
     let file;
